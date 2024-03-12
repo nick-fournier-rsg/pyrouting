2a85304c45a70338ca5bb33f1fd60415b15d7c9a
Nick Fournier <nick.fournier@rsginc.com>
2024-03-11 18:26:09 -0700

    another pat typo in run scripts

--- scripts/osrm/docker_run.bat
+++ scripts/osrm/docker_run.bat
@@ -3,5 +3,5 @@
 set osm_area="us-west-latest"
 
 docker run -t -i -p 5000:5000 -v "%cd%/data/car:/data/car" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/car/%osm_area%.osrm &
-docker run -t -i -p 5001:5001 -v "%cd%/data/bike:/data/bike" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/bike/%osm_area%.osrm &
+docker run -t -i -p 5001:5001 -v "%cd%/data/bicycle:/data/bike" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/bicycle/%osm_area%.osrm &
 docker run -t -i -p 5002:5002 -v "%cd%/data/foot:/data/foot" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/foot/%osm_area%.osrm &
--- scripts/osrm/docker_run.sh
+++ scripts/osrm/docker_run.sh
@@ -2,5 +2,5 @@
 osm_area="us-west-latest"
 
 docker run -t -i -p 5000:5000 -v "${PWD}/data/car:/data/car" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/car/${osm_area}.osrm &
-docker run -t -i -p 5001:5001 -v "${PWD}/data/bike:/data/bike" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/bike/${osm_area}.osrm &
+docker run -t -i -p 5001:5001 -v "${PWD}/data/bicycle:/data/bike" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/bicycle/${osm_area}.osrm &
 docker run -t -i -p 5002:5002 -v "${PWD}/data/foot:/data/foot" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/foot/${osm_area}.osrm &
